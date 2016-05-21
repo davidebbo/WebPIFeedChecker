7c2de8ace1c0c2b76bbad9a498d4ce6bd4141984
davidebbo <david.ebbo@microsoft.com>
2016-05-21 11:05:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPIFeedChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
