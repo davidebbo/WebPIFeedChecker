a0b16e180a6b84a8f28464b1cd53cc4f3e9ddb31
davidebbo <david.ebbo@microsoft.com>
2016-05-23 18:01:12 -0700

    Support multiple feeds and global dependencies

--- Program.cs
+++ Program.cs
@@ -12,51 +12,70 @@ namespace WebPIFeedChecker
 
         static void Main(string[] args)
         {
-            if (args.Length < 1 || args.Length > 2)
+            if (args.Length < 1)
             {
-                Console.WriteLine("Syntax: WebPIFeedChecker.exe FeedFile [PackageFolder]");
+                Console.WriteLine("Syntax: WebPIFeedChecker.exe roots feed [feed...]");
                 return;
             }
 
-            string feedFile = args[0];
-            string packageFolder = args.Length > 1 ? args[1] : null;
-
-            var reader = new StreamReader(feedFile, true);
-
-            var root = XElement.Load(reader);
-            XNamespace ns = "http://www.w3.org/2005/Atom";
-
-
-            var entries = from entry in root.Descendants(ns + "entry")
-                          select new FeedEntry
-                          {
-                              Id = entry.Element(ns + "productId").Value,
-                              Dependencies = from dependency in entry.Descendants(ns + "dependency")
-                                             select dependency.Descendants(ns + "productId").First().Value
-                          };
+            var entries = args.Skip(1).SelectMany(ReadFeed);
             _entries = entries.ToDictionary(e => e.Id);
 
-
-            // First entry is the root node
-            WalkFeedEntry(_entries.Values.First());
-
-            foreach (var element in root.Element(ns + "featuredProducts").Elements(ns + "productId"))
+            string[] roots = File.ReadAllLines(args[0]);
+            foreach (string root in roots)
             {
                 FeedEntry entry;
-                if (_entries.TryGetValue(element.Value, out entry))
+                if (!_entries.TryGetValue(root, out entry))
                 {
-                    WalkFeedEntry(entry);
+                    throw new ArgumentException($"Can't find root {roots}");
                 }
-                else
-                {
-                    Console.WriteLine("Missing featured product: " + element.Value);
-                }
+
+                WalkFeedEntry(entry);
             }
 
+            Console.WriteLine("Unused products:");
             foreach (var entry in _entries.Values.Where(e => !e.Reachable))
             {
                 Console.WriteLine($"{entry.Id}");
             }
+        }
+
+        static IEnumerable<FeedEntry> ReadFeed(string feedFile)
+        {
+            var reader = new StreamReader(feedFile, detectEncodingFromByteOrderMarks: true);
+
+            var root = XElement.Load(reader);
+            XNamespace ns = "http://www.w3.org/2005/Atom";
+
+            XElement dependenciesNode = root.Element(ns + "dependencies") ?? new XElement("dummy");
+
+            var topDeps = from dependency in dependenciesNode.Elements(ns + "dependency")
+                          select new
+                          {
+                              Id = dependency.Attribute("id").Value,
+                              Dependencies = from productId in dependency.Descendants(ns + "productId")
+                                             select productId.Value
+                          };
+            var topDepsDict = topDeps.ToDictionary(e => e.Id);
+
+            Func<XElement, IEnumerable<string>> productsIds = (dependency) =>
+            {
+                var productId = dependency.Descendants(ns + "productId").FirstOrDefault();
+                if (productId == null)
+                {
+                    string idref = dependency.Attribute("idref").Value;
+                    return topDepsDict[idref].Dependencies;
+                }
+
+                return Enumerable.Repeat(productId.Value, 1);
+            };
+
+            return from entry in root.Descendants(ns + "entry")
+                   select new FeedEntry
+                   {
+                       Id = entry.Element(ns + "productId").Value,
+                       Dependencies = entry.Descendants(ns + "dependency").SelectMany(productsIds)
+                   };
         }
 
         static void WalkFeedEntry(FeedEntry entry)
